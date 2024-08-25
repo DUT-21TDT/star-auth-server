6c564842829de2e3fb1929a3bd57ad1e985fe07e
lightx26 <ncnhatquang26@gmail.com>
2024-08-25 10:59:05 +0700

    Initial: star authorization server

---
+++ src/main/java/com/pbl/starauthserver/StarAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.pbl.starauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarAuthServerApplication.class, args);
+    }
+
+}
