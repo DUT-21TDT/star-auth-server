b2f2890bbb72976a102e235507db9c0c2424e42e
lightx26 <ncnhatquang26@gmail.com>
2024-08-25 15:00:12 +0700

    SSMP-72: Add role claims to JWT

---
+++ src/main/java/com/pbl/starauthserver/configurations/JwtConfig.java
@@ -0,0 +1,29 @@
+package com.pbl.starauthserver.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+
+import java.util.stream.Collectors;
+
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtTokenCustomizer() {
+        return (context) -> {
+            if (OAuth2TokenType.ACCESS_TOKEN.equals(context.getTokenType())) {
+                context.getClaims().claims((claims) -> {
+                    String roles = context.getPrincipal().getAuthorities().stream()
+                            .map(Object::toString)
+                            .collect(Collectors.joining(" "));
+
+                    claims.put("role", roles);
+                });
+            }
+        };
+    }
+
+}
